7874474b82fa3ba67fd1903c692a7d0eeff5c359
Joabe Oliveira <oliveira_joabe@hotmail.com>
2025-04-14 13:01:55 -0300

    correção de layout

--- src/components/DefaultLayout/index.tsx
+++ src/components/DefaultLayout/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 
 export default function DefaultLayout() {
   return (
-    <main className="bg-black text-white min-h-screen">
+    <main className="bg-black text-white min-h-screen ">
       <Navbar />
       <div className="sm:px-20 px-8">
         <Outlet />
