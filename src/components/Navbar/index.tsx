5a2f23aa4f9e62a66e231102eb1f308489a720e4
Joabe Oliveira <oliveira_joabe@hotmail.com>
2025-04-14 12:16:35 -0300

    add logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,11 @@
+import logo from "../../assets/img/logo1.png";
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between sm:px-20 px-8 pt-4">
-      <span>logo</span>
+      <span>
+        <img src={logo} alt="logo" className="w-9 h-9" />
+      </span>
       <ul className="flex gap-4 sm:gap-8">
         <li>Login</li>
         <li>Cadastro</li>
